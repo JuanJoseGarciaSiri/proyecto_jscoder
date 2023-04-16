41eeec1c86e9f85ab6ddb992a40467e1b43956f4
JuanJoseGarciaSiri <juanjogarciasiri22.com>
2023-04-15 23:53:31 -0300

    primeros pasos

---
+++ main.js
@@ -0,0 +1,25 @@
+
+
+
+
+let nombre1 = "juan ";
+let nombre2 = "jose";
+let nombreCompleto = nombre1+nombre2;
+
+alert("hola soy " + nombreCompleto);
+
+const NUMERO1= 12;
+const NUMERO2= 13;
+
+let total= NUMERO1 + NUMERO2;
+console.log("el numero es" + total);
+let numero = Number (prompt("ingrese un numero "));
+
+let nombre = prompt("ingrese su nombre");
+console.log("hola"+ nombre);
+
+
+let numeros= parseInt(prompt("ingrese un numero"));
+if (numero > 1000){
+    alert("su numero es mayor a 1000");
+}

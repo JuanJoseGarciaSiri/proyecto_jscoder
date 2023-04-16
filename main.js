7eaa7a99282df0c598d22373f11c29199f9f5218
JuanJoseGarciaSiri <juanjogarciasiri22.com>
2023-04-16 17:12:00 -0300

    arreglo detalles, agrego un for y una funcion

--- main.js
+++ main.js
@@ -1,23 +1,27 @@
 
+for (let i = 3; i>0 ; i--){
+  alert("Ingresa a nuestro sitio en "+i);
+}
 let nombre= prompt("Ingresa tu nombre");
 alert("Bienvenido/a "+nombre+ "!");
 
-let edad= isNaN(Number(prompt("¿Cual es tu edad "+nombre+"?")));
+let edad= isNaN(Number(prompt("¿Cuál es tu edad "+nombre+"?")));
 
 const MAYOR=18;
-
 if (edad >= MAYOR){
-    alert("Bienvenido/a a nuestra tienda "+nombre+" aqui vas a encontrar todos los talles de adulto y equipaciones deportivas para que rindas a tu maximo potencial");
+    alert("Bienvenido/a a nuestra tienda "+nombre+" aquí vas a encontrar todos los talles de adulto y equipaciones deportivas para que rindas a tu maximo potencial");
 
 }
-else if (edad < MAYOR){
-    alert("Bienvenido/a a nuestra tienda "+nombre+" tenemos todos los talles junior para todos los deportes en categorias inferiores");
+    else if (edad < MAYOR){
+        alert("Bienvenido/a a nuestra tienda "+nombre+" tenemos todos los talles junior para todos los deportes en categorías inferiores");
+    }
+        else {
+            alert("Debes ingresar un numero si quieres nuestras recomendaciones");
 }
-else {
-    alert("Debes ingresar un numero si quieres nuestras recomendaciones");
-}
+    
 
-let favorito=prompt("¿Cual es tu deporte favorito?");
+
+let favorito=prompt("¿Cuál es tu deporte favorito?");
 
 while (favorito != "esc"){
 switch (favorito){
@@ -25,19 +29,19 @@ switch (favorito){
         break
         case "Basketball" : alert("Te presentamos la camiseta autografiada por Kevin Durant");
         break
-        case "Automovilismo": alert("Tenemos las equipaciones de todas las escuderias");
+        case "Automovilismo": alert("Tenemos las equipaciones de todas las escuderías");
         break
         case "Tenis" : alert("Raquetas, pelotas, lo que necesites, lo tenemos");
         break 
         case "Futbol Americano": alert(" Todos los jerseys de la NFL pueden ser tuyos");
         break
-        case "Rugby": alert("Las camisetas de Los Teros, Las Cimarronas, Los Pumas, Las Leonas, pueden ser tuyas, pasa a echarles un vistazo");
+        case "Rugby": alert("Las camisetas de Los Teros, Las Cimarronas, Los Pumas, Las Leonas, pueden ser tuyas, pasá a echarles un vistazo");
         break
 
         default : alert("Deporte no encontrado, actualmente trabajamos con Futbol, Basketball, Automovilismo, Tenis, Futbol Americano y Rugby, no olvides las Mayusculas ,ingresa esc si quieres continuar al sitio ");
         break;
     }
-favorito=prompt("¿Cual es tu deporte favorito?");
+    favorito=prompt("¿Cuál es tu deporte favorito?");
 }
 
 let locales=Number(prompt("ingrese la cantidad de torneos locales de su equipo favorito"));
@@ -46,9 +50,8 @@ let mundiales= Number(prompt("ingrese la cantidad de torneos mundiales de su equ
 let resultado=0;
 
 function gloriasDeTuEquipo (locales, continentales, mundiales){
-resultado = locales + continentales + mundiales;
-
-return (alert("Su equipo tiene un palmares de "+resultado+" trofeos"));
+    resultado = locales + continentales + mundiales;
+    return (alert("Su equipo tiene un palmarés de "+resultado+" trofeos"));
 }
 
 gloriasDeTuEquipo(locales, continentales, mundiales);

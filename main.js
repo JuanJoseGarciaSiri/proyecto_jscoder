11e3210e9edf4bb679750ea63bef4d9a448f34bf
JuanJoseGarciaSiri <juanjogarciasiri22.com>
2023-05-22 02:17:37 -0300

    agrego eventos y funciones

--- main.js
+++ main.js
@@ -18,51 +18,75 @@ items[3].innerHTML="Contacto";
 ///SECCIONES///
 
 //SECCION DEPORTES//
-let listaDep = document.createElement("listaDep");
+let listaDep = document.createElement("ul");
 listaDep.innerHTML="<li> Futbol </li> <li> Basketball </li> <li> Rugby </li> <li> Hockey </li> <li> Futbol Americano</li> ";
 let seclistaDep = document.getElementsByClassName("listaDep")[0];
 seclistaDep.appendChild(listaDep);
 
-let deportes = document.createElement("div");
-deportes.innerHTML="<h2>DEPORTES</h2>";
+let deportes = document.createElement("h3");
+deportes.innerHTML="<h3>DEPORTES</h3>";
 let secdeportes = document.getElementsByClassName("deportes")[0];
 secdeportes.appendChild(deportes);
 
 //SECCION INDUMENTARIA//
-let listaInd = document.createElement("listaInd");
+let listaInd = document.createElement("ul");
 listaInd.innerHTML="<li> Premier League</li><li> liga Uruguaya</li><li> liga Argentina</li><li> liga española</li><li> Brasileirao </li><li> NBA </li><li> NFL </li><li> Superliga Rugby </li>";
 let listaubicacion= document.getElementsByClassName("listaIN")[0];
 listaubicacion.appendChild(listaInd);
 
-let ind = document.createElement("div");
-ind.innerHTML="<h2>INDUMENTARIAS</h2>";
+let ind = document.createElement("h3");
+ind.innerHTML="<h3>INDUMENTARIAS</h3>";
 let indumen= document.getElementsByClassName("indu")[0];
 indumen.appendChild(ind);
 
 //SECCION CALZADO//
-let listaCalz = document.createElement("listaCalz");
+let listaCalz = document.createElement("ul");
 listaCalz.innerHTML="  <li> Nike </li><li> Adidas </li><li> Puma </li><li> Umbro </li><li> Topper </li>";
 let listaubCalz= document.getElementsByClassName("listaC")[0];
 listaubCalz.appendChild(listaCalz);
 
-let cal = document.createElement("cal");
-cal.innerHTML="<h2>CALZADOS</h2>";
+let elementoLista = document.getElementsByClassName("listaC")[0].querySelector("li");
+
+elementoLista.addEventListener('mouseover', function(event) {
+    console.log("el usuario esta viendo el calzado",event);
+});
+
+let cal = document.createElement("h3");
+cal.innerHTML="<h3>CALZADOS</h3>";
 let ubcal= document.getElementsByClassName("calzado")[0];
 ubcal.appendChild(cal);
 
 //SECCION ACCESORIOS//
-let listaAc=document.createElement("listaAc");
+let listaAc=document.createElement("ul");
 listaAc.innerHTML="<li> Palos de Hockey</li><li> Medias Deportivas</li><li> Pelotas de Futbol</li><li> Pelotas de Rugby</li><li> Balon de NFL</li>";
 let ubiLista=document.getElementsByClassName("listAccesorios")[0];
 ubiLista.appendChild(listaAc);
 
-let acces= document.createElement("acces");
-acces.innerHTML="<h2>ACCESORIOS</h2>";
+let acces= document.createElement("h3");
+acces.innerHTML="<h3>ACCESORIOS</h3>";
 let ubiAc = document.getElementsByClassName("accesorios")[0];
 ubiAc.appendChild(acces);
 
 //SECCION FORMULARIOS
-let formTitulo= document.createElement("formT");
-formTitulo.innerHTML="<h2>REGISTRO PARA COMPRA</h2>";
+let formTitulo= document.createElement("h3");
+formTitulo.innerHTML="<h3>REGISTRO PARA COMPRA</h3>";
 let ubiTitulo = document.getElementsByClassName("formT")[0];
 ubiTitulo.appendChild(formTitulo);
+
+let form = document.getElementById("formulario");
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    console.log("Su formulario ha sido enviado");
+});
+
+ boton.onmousemove = () => {
+   console.log("el mouse se esta moviendo, el usuario no esta afk");
+ }
+
+let formul = document.getElementById("formulario");
+form.addEventListener('change', function(evento) {
+    let cambiado = evento.target;
+    console.log("El elemento ha sido modificado: ", cambiado);
+});
+
+//STORAGE///
